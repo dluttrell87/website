01521b4b36976598e38cf93ec4dde8b0d248ea2f
alpha <dluttrell87@gmail.com>
2022-04-20 16:41:06 -0400

    Hello World!

--- test/GUI_test.py
+++ test/GUI_test.py
@@ -1,26 +1,83 @@
+#!/usr/bin/env python3
 import PySimpleGUI as sg
-import os.path
+
+class datastore:
+
+    def __init__(self, x, y, z):
+        print('init called')
+        self.usage = x
+        self.request = y
+        self.size = z
+
+
+    def getPercent(self):
+        return (self.size-self.usage)/self.size*100
+
+    def getNewPercent(self):
+        return (self.size-(self.usage+self.request))/self.size*100
+
+    def needsExpansion(self):
+        if (self.size-(self.usage+self.request))/self.size*100 < 20:
+            return True
+        else:
+            return False
+
+
+    def getTotal(self):
+        totalsize = self.size
+        while (totalsize-(self.usage+self.request))/totalsize*100 < 20:
+            totalsize += 1
+        return totalsize
+
+
+    def getGB(self):
+        targetsize = self.size
+        while (targetsize-(self.usage+self.request))/targetsize*100 < 20:
+            targetsize += 1
+        return targetsize-size
+
+
 sg.theme('DarkGrey7')
-#sg.Window(title="poop", layout=[[]], margins=(100, 50)).read()
 
 layout = [
-    [sg.Text('Enter datastore information:')],
-    [sg.Text('Current used space', size=(15, 1)), sg.InputText()],
-    [sg.Text('How much to add', size=(15, 1)), sg.InputText()],
-    [sg.Text('Current total', size=(15, 1)), sg.InputText()],
-    [sg.Submit(), sg.Cancel()]
+    [sg.Text('Enter datastore information:', font='Tahoma')],
+    [sg.Text('Current used space', font='Tahoma', size=(14, 1)), sg.InputText()],
+    [sg.Text('How much to add', font='Tahoma', size=(14, 1)), sg.InputText()],
+    [sg.Text('Current total', font='Tahoma', size=(14, 1)), sg.InputText()],
+    [sg.Submit('Calculate', font='Tahoma'), sg.Cancel('Cancel', font='Tahoma')]
 ]
 
 window = sg.Window("Datastore Allocation", layout)
 event, values = window.read()
-
-
 window.close()
 
-layout = [
-    [sg.Text("Increase datastore by")],
-    [sg.Text(values[0])]
-]
+usage = int(values[0])
+request = int(values[1])
+size = int(values[2])
 
-window = sg.Window("Results", layout)
-event, values = window.read()
+Datastore = datastore(usage, request, size)
+
+if Datastore.needsExpansion() is True:
+    layout = [
+        [sg.Text("Increase datastore by:", font='Tahoma')],
+        [sg.Text(Datastore.getGB(), font='Tahoma')],
+        [sg.Text("Datastore total size should be:", font='Tahoma')],
+        [sg.Text(Datastore.getTotal(), font='Tahoma')],
+        [sg.Submit("OK", font='Tahoma')]
+    ]
+
+    window = sg.Window("Results", layout)
+    event, values = window.read()
+    window.close()
+else:
+    print("test")
+    layout = [
+        [sg.Text("No increase necessary", font='Tahoma')],
+        [sg.Text("New Percent used:", font='Tahoma')],
+        [sg.Text(Datastore.getNewPercent(), font='Tahoma')],
+        [sg.Submit("OK", font='Tahoma')]
+    ]
+
+    window = sg.Window("Results", layout)
+    event, values = window.read()
+    window.close()
